77a30e4bcc867be1f362e31d3cbef573c81a2307
Jayanth <jayanth@Users-MacBook-Air.local>
2021-08-04 12:20:11 +0530

    second commit

---
+++ src/components/Form/Form.js
@@ -0,0 +1,55 @@
+
+import React ,{useState,useEffect}from 'react';
+import {TextField,Button,Typography,Paper} from '@material-ui/core'
+import FileBase from 'react-file-base64'
+import useStyles from "./styles";
+import { useDispatch,useSelector } from 'react-redux';
+
+import { createPost } from '../../actions/posts';
+import { updatePost } from '../../actions/posts'
+const Form = ({currentId,setCurrentId}) => {
+        const classes = useStyles();
+        const dispatch =  useDispatch();
+        const [postData,setpostData] = useState(
+            {creator:"", title:"", message:"", tags:"", selectedFile:""}
+        );
+        const post = useSelector((state)=> currentId ? state.posts.find((p)=>p._id===currentId):null)
+        
+        useEffect(()=>{
+            if(post) setpostData(post);
+        },[post])
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            if(currentId){
+                dispatch(updatePost(currentId,postData));                
+            }
+            else{
+                dispatch(createPost(postData));               
+            }
+            clear();
+        }
+        const clear = () => {
+            setCurrentId(null);
+            setpostData({creator:"", title:"", message:"", tags:"", selectedFile:""});
+        }
+
+    return(
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography variant="h6">{currentId ?`edit`:`share`} your experience</Typography>
+                <TextField name="creator" variant="outlined" label="creator" value={postData.creator} onChange={(e)=>setpostData({...postData,creator:e.target.value})} fullWidth> </TextField>
+                <TextField name="title" variant="outlined" label="title" value={postData.title} onChange={(e)=>setpostData({...postData,title:e.target.value})} fullWidth> </TextField>
+                <TextField name="message" variant="outlined" label="message" value={postData.message} onChange={(e)=>setpostData({...postData,message:e.target.value})} fullWidth> </TextField>
+                <TextField name="tags" variant="outlined" label="tags" value={postData.tags} onChange={(e)=>setpostData({...postData,tags:e.target.value})} fullWidth> </TextField>
+            <div className={classes.fileInput}>
+                <FileBase type="file" multiple={false} onDone={({base64})=> setpostData({...postData,selectedFile:base64})}></FileBase>
+            </div>
+            <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>submit</Button>
+            <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>clear</Button>
+            </form>
+
+
+        </Paper>
+    )
+}
+export default Form;
---
+++ src/reducers/posts.js
@@ -0,0 +1,20 @@
+
+// a reducer is a function that accepts the state and action
+export default (posts=[],action) => {
+    switch (action.type){
+        case 'UPDATE':
+            console.log('update');
+            return posts.map((post)=>post._id===action.payload._id?action.payload:post);
+        case 'FETCH_ALL':
+            console.log("fetch")
+            return action.payload;
+        case 'CREATE':
+            console.log('post');
+            return [...posts,action.payload];
+
+        default :
+            return posts;
+
+    }
+        
+}
